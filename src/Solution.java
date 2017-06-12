e067e647ff5842b4313f2df75af85deeac97b45e
Feiteng <li.feiteng@gmail.com>
2017-06-12 09:31:15 -0400

    u

--- src/Solution.java
+++ src/Solution.java
@@ -49,15 +49,135 @@ public class Solution
 
 		time = System.currentTimeMillis();
 
-		String[] pStrings = { "root/a 1.txt(abcd) 2.txt(efsfgh)", "root/c 3.txt(abdfcd)", "root/c/d 4.txt(efggdfh)" };
-		System.out.println( s.findDuplicate( pStrings ) );
+		int[] m = { 66, 90, 7, 6, 32, 16, 2, 78, 69, 88, 85, 26, 3, 9, 58, 65, 30, 96, 11, 31, 99, 49, 63, 83, 79, 97, 20, 64, 81, 80, 25, 69, 9, 75,
+				23, 70, 26, 71, 25, 54, 1, 40, 41, 82, 32, 10, 26, 33, 50, 71, 5, 91, 59, 96, 9, 15, 46, 70, 26, 32, 49, 35, 80, 21, 34, 95, 51, 66,
+				17, 71, 28, 88, 46, 21, 31, 71, 42, 2, 98, 96, 40, 65, 92, 43, 68, 14, 98, 38, 13, 77, 14, 13, 60, 79, 52, 46, 9, 13, 25, 8 };
+
+		System.out.println( s.canPartition( m ) );
+
 		System.out.printf( "Run time... %s ms", System.currentTimeMillis() - time );
 
 	}
 
-	public int integerReplacement( int n )
+	boolean canPartition( int[] m )
 	{
+		int s = 0;
+		for ( int n : m )
+			s += n;
+		if ( ( s & 1 ) != 0 )
+			return false;
+		int[] nums = new int[m.length + 1];
+		System.arraycopy( m, 0, nums, 0, m.length );
+
+		long[] vals = new long[s + 1];
+
+		vals[0] = 1;
+		Arrays.sort( nums );
+
+		int runSum = 1, k = 0;
+
+		vals[0] = 1;
+		for ( int i = 1; i < nums.length; i++ )
+		{
+			runSum += nums[k];
+			// System.out.printf( "%d %d \n", i, runSum );
+			for ( int j = runSum - 1; j >= 0; j-- )
+				vals[nums[i] + j] += vals[j];
+			System.out.println( Arrays.toString( vals ) );
+			k++;
+		}
+
+		return vals[s / 2] > 0 && ( vals[s / 2] & 1 ) == 0;
+
+	}
+
+	int numberOfPatterns_count = 0;
+
+	public int numberOfPatterns( int m, int n )
+	{
+
+		int[][] pos = { { 0, 0 }, { 0, 1 } };
+
+		for ( int i = m; i <= n; i++ )
+		{
+			for ( int[] p : pos )
+			{
+				countPattern( i, new boolean[3][3], p[0], p[1] );
+				System.out.println( numberOfPatterns_count );
+			}
+		}
+		numberOfPatterns_count *= 4;
+		for ( int i = m; i <= n; i++ )
+			countPattern( i, new boolean[3][3], 1, 1 );
+
+		return numberOfPatterns_count;
+	}
+
+	void countPattern( int n, boolean[][] visit, int i, int j )
+	{
+		visit[i][j] = true;
+		if ( n == 1 )
+		{
+			numberOfPatterns_count++;
+			return;
+		}
+		int[][] dirsD = { { -1, -1 }, { -1, 0 }, { -1, 1 },
+				{ 0, -1 }, { 0, 1 },
+				{ 1, -1 }, { 1, 0 }, { 1, 1 },
+				{ -2, -2 }, { -2, 0 }, { -2, 2 },
+				{ 0, -2 }, { 0, 2 },
+				{ 2, -2 }, { 2, 0 }, { 2, 2 },
+				{ -1, -2 }, { -1, 2 }, { 1, -2 }, { 1, 2 },
+				{ -2, -1 }, { -2, 1 }, { 2, -1 }, { 2, 1 }
+		};
+		int ni, nj;
+		for ( int[] d : dirsD )
+		{
+			ni = d[0] + i;
+			nj = d[1] + j;
+
+			if ( ni < 0 || nj < 0 || ni >= 3 || nj >= 3 || visit[ni][nj] )
+				continue;
+			if ( checkMid( visit, i, j, ni, nj ) )
+				continue;
+			visit[ni][nj] = true;
+			countPattern( n - 1, visit, ni, nj );
+			visit[ni][nj] = false;
+		}
+	}
+
+	boolean checkMid( boolean[][] visit, int i, int j, int ni, int nj )
+	{
+		if ( Math.abs( ni - i ) <= 1 && Math.abs( nj - j ) <= 1 )
+			return false;
+		// return true if mid has not been visited
+		if ( Math.abs( ni - i ) > 1 && Math.abs( nj - j ) == 1 || Math.abs( nj - j ) > 1 && Math.abs( ni - i ) == 1 )
+			return false;
+		int fi = ( ni - i ) / 2, fj = ( nj - j ) == 0 ? 0 : nj > j ? 1 : -1;
+		return !visit[i + fi][j + fj];
+	}
+
+	public int integerReplacement( int m )
+	{
+
 		int k = 0;
+		long n = m;
+		while ( n > 1 )
+		{
+			while ( ( n & 1 ) == 0 )
+			{
+				n >>= 1;
+				k++;
+			}
+			if ( n == 1 )
+				break;
+			if ( Long.bitCount( n - 1 ) < Long.bitCount( n + 1 ) || n == 3 )
+				n--;
+			else
+				n++;
+
+			k++;
+		}
 
 		return k;
 	}
