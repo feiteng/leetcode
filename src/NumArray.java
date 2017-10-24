d3867d59d7d63f081486f8da4da28956971398af
Feiteng <li.feiteng@gmail.com>
2017-10-24 17:57:31 -0400

    1024

--- src/NumArray.java
+++ src/NumArray.java
@@ -1,89 +1,145 @@
 class NumArray
 {
-	SegmentTree _root;
+	int[] vals, BIT;
 	int n;
-
-	class SegmentTree
-	{
-		int _lBound = 0, _rBound = 0, _sum = 0;
-		SegmentTree _left = null, _right = null;
-	}
-
-	void printSegTree( SegmentTree t )
-	{
-		if ( t == null )
-			return;
-		if ( t._lBound == t._rBound )
-			System.out.printf( " %d", t._sum );
-		printSegTree( t._left );
-		printSegTree( t._right );
-	}
-
-	SegmentTree buidSegTree( int[] vals, int left, int right )
-	{
-		SegmentTree re = new SegmentTree();
-		if ( left == right )
-		{
-			re._lBound = re._rBound = left;
-			re._sum = vals[left];
-			return re;
-		}
-		if ( left > right )
-			return null;
-		int mid = ( left + right ) / 2;
-		re._lBound = left;
-		re._rBound = right;
-		re._left = buidSegTree( vals, left, mid );
-		re._right = buidSegTree( vals, mid + 1, right );
-		if ( left == right )
-			re._sum = vals[left];
-		else
-			re._sum = re._left._sum + re._right._sum;
-		return re;
-	}
 
 	public NumArray( int[] nums )
 	{
-		n = nums.length;
-		_root = buidSegTree( nums, 0, n - 1 );
+		vals = nums;
+		BIT = new int[n + 1];
+		n = vals.length;
+		for ( int i = 0; i < n; i++ )
+		{
+			constructBIT( i, vals[i] );
+		}
+	}
+
+	void constructBIT( int i, int val )
+	{
+		i++;
+		while ( i <= n )
+		{
+			BIT[i] += val;
+			i += ( i & -i );
+		}
+
 	}
 
 	public void update( int i, int val )
 	{
-		updateSegTree( i, val, _root );
-	}
-
-	void updateSegTree( int i, int val, SegmentTree t )
-	{
-		if ( t._lBound == i && t._rBound == i )
-		{
-			t._sum = val;
-			return;
-		}
-		int m = ( t._lBound + t._rBound ) / 2;
-		if ( i <= m )
-			updateSegTree( i, val, t._left );
-		else
-			updateSegTree( i, val, t._right );
-		t._sum = t._left._sum + t._right._sum;
+		constructBIT( i, val - vals[i] );
+		vals[i] = val;
 	}
 
 	public int sumRange( int i, int j )
 	{
-		return sumHelper( 0, n - 1, i, j, _root );
+		return sumBIT( j ) - sumBIT( i );
 	}
 
-	int sumHelper( int L, int R, int i, int j, SegmentTree p )
+	int sumBIT( int k )
 	{
-		if ( j < L || R < i )
-			return 0;
-		if ( i <= L && R <= j )
-			return p._sum;
-		int M = ( L + R ) / 2;
-		return sumHelper( L, M, i, j, p._left ) +
-				sumHelper( M + 1, R, i, j, p._right );
+		k++;
+		int re = 0;
+		while ( k > 0 )
+		{
+			re += BIT[k];
+			k -= ( k & -k );
+		}
+		return re;
 	}
 
+	/**
+	 * Your NumArray object will be instantiated and called as such:
+	 * NumArray obj = new NumArray(nums);
+	 * obj.update(i,val);
+	 * int param_2 = obj.sumRange(i,j);
+	 */
+	// class NumArray
+	// {
+	// SegmentTree _root;
+	// int n;
+	//
+	// class SegmentTree
+	// {
+	// int _lBound = 0, _rBound = 0, _sum = 0;
+	// SegmentTree _left = null, _right = null;
+	// }
+	//
+	// void printSegTree( SegmentTree t )
+	// {
+	// if ( t == null )
+	// return;
+	// if ( t._lBound == t._rBound )
+	// System.out.printf( " %d", t._sum );
+	// printSegTree( t._left );
+	// printSegTree( t._right );
+	// }
+	//
+	// SegmentTree buidSegTree( int[] vals, int left, int right )
+	// {
+	// SegmentTree re = new SegmentTree();
+	// if ( left == right )
+	// {
+	// re._lBound = re._rBound = left;
+	// re._sum = vals[left];
+	// return re;
+	// }
+	// if ( left > right )
+	// return null;
+	// int mid = ( left + right ) / 2;
+	// re._lBound = left;
+	// re._rBound = right;
+	// re._left = buidSegTree( vals, left, mid );
+	// re._right = buidSegTree( vals, mid + 1, right );
+	// if ( left == right )
+	// re._sum = vals[left];
+	// else
+	// re._sum = re._left._sum + re._right._sum;
+	// return re;
+	// }
+	//
+	// public NumArray( int[] nums )
+	// {
+	// n = nums.length;
+	// _root = buidSegTree( nums, 0, n - 1 );
+	// }
+	//
+	// public void update( int i, int val )
+	// {
+	// updateSegTree( i, val, _root );
+	// }
+	//
+	// void updateSegTree( int i, int val, SegmentTree t )
+	// {
+	// if ( t._lBound == i && t._rBound == i )
+	// {
+	// t._sum = val;
+	// return;
+	// }
+	// int m = ( t._lBound + t._rBound ) / 2;
+	// if ( i <= m )
+	// updateSegTree( i, val, t._left );
+	// else
+	// updateSegTree( i, val, t._right );
+	// t._sum = t._left._sum + t._right._sum;
+	// }
+	//
+	// public int sumRange( int i, int j )
+	// {
+	// return sumHelper( 0, n - 1, i, j, _root );
+	// }
+	//
+	// int sumHelper( int L, int R, int i, int j, SegmentTree p )
+	// {
+	// if ( j < L || R < i )
+	// return 0;
+	// if ( i <= L && R <= j )
+	// return p._sum;
+	// int M = ( L + R ) / 2;
+	// return sumHelper( L, M, i, j, p._left ) +
+	// sumHelper( M + 1, R, i, j, p._right );
+	// }
+	//
 	public static void main( String[] args )
 	{
 
@@ -102,16 +158,16 @@ class NumArray
 				na.update( n[i][0], n[i][1] );
 				System.out.println( "null" );
 			}
-			na.printSegTree( na._root );
+			// na.printSegTree( na._root );
 			System.out.println();
 		}
 	}
 
 }
 
-/**
- * Your NumArray object will be instantiated and called as such:
- * NumArray obj = new NumArray(nums);
- * obj.update(i,val);
- * int param_2 = obj.sumRange(i,j);
- */
+/// **
+// * Your NumArray object will be instantiated and called as such:
+// * NumArray obj = new NumArray(nums);
+// * obj.update(i,val);
+// * int param_2 = obj.sumRange(i,j);
+// */
